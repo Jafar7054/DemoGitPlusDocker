a1406dd7ed6b2ed27b948166cecf43b39a610218
Jafar7054 <119682032+Jafar7054@users.noreply.github.com>
2025-10-19 16:08:57 +0530

    Update DemoDockerPlusGitApplication.java
    updated from Controller to RestController

--- src/main/java/com/dockerPlusGit/DemoDockerPlusGitApplication.java
+++ src/main/java/com/dockerPlusGit/DemoDockerPlusGitApplication.java
@@ -2,11 +2,11 @@ package com.dockerPlusGit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class DemoDockerPlusGitApplication {
 
 	public static void main(String[] args) {
