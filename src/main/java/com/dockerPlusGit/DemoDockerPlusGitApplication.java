81f01c4c14aeb2c31a450d08391f333895b024e9
Jafar7054 <119682032+Jafar7054@users.noreply.github.com>
2025-10-19 16:04:10 +0530

    Update DemoDockerPlusGitApplication.java
    adding getMethod

--- src/main/java/com/dockerPlusGit/DemoDockerPlusGitApplication.java
+++ src/main/java/com/dockerPlusGit/DemoDockerPlusGitApplication.java
@@ -3,6 +3,7 @@ package com.dockerPlusGit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
@@ -11,7 +12,8 @@ public class DemoDockerPlusGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDockerPlusGitApplication.class, args);
 	}
-	
+
+	@GetMapping("/")
 	public String printMsg()
 	{
 		return "Demo Test Success";
